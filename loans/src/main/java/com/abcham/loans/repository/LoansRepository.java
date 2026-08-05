c5280a0bdc5325e0dedcbe145cf23ced09812c15
cham207388 <cham207388@gmail.com>
2026-08-05 10:42:37 -0500

    loans microservice

---
+++ loans/src/main/java/com/abcham/loans/repository/LoansRepository.java
@@ -0,0 +1,14 @@
+package com.abcham.loans.repository;
+
+import com.abcham.loans.entity.Loans;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface LoansRepository extends JpaRepository<Loans, Long> {
+
+    Optional<Loans> findByMobileNumber(String mobileNumber);
+
+    Optional<Loans> findByLoanNumber(String loanNumber);
+
+}
