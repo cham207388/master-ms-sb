4478a733830f124c7d20835e6547cd7dc9699a65
cham207388 <cham207388@gmail.com>
2026-08-06 18:42:55 -0500

    feat: initialize Spring Cloud Config Server and add DeclarableCustomizer to resolve RabbitMQ queue declaration issues

---
+++ config-server/src/main/java/com/abcham/configserver/ConfigServerApplication.java
@@ -0,0 +1,28 @@
+package com.abcham.configserver;
+
+import org.springframework.amqp.core.DeclarableCustomizer;
+import org.springframework.amqp.core.Queue;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+    @Bean
+    public DeclarableCustomizer queueLeaderLocatorRemover() {
+        return declarable -> {
+            if (declarable instanceof Queue queue) {
+                queue.getArguments().remove("x-queue-leader-locator");
+                queue.getArguments().remove("x-queue-master-locator");
+            }
+            return declarable;
+        };
+    }
+}
