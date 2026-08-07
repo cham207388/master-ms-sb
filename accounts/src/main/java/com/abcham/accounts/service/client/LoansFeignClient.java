98ba0d21f362697bf77cbe6e3387d2dcd44353ef
cham207388 <cham207388@gmail.com>
2026-08-07 12:34:27 -0500

    hard code

--- accounts/src/main/java/com/abcham/accounts/service/client/LoansFeignClient.java
+++ accounts/src/main/java/com/abcham/accounts/service/client/LoansFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "loans", url = "${loans.service.base.path}")
+@FeignClient(name = "loans", path = "/api/loans")
 public interface LoansFeignClient {
 
     @GetMapping(value = "/fetch", consumes = "application/json")
