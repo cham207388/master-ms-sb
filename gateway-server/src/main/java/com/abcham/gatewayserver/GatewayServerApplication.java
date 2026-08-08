a11dec17871db2e063655ff9328c88eb445dbe2e
cham207388 <cham207388@gmail.com>
2026-08-08 06:49:44 -0500

    feat: implement custom routing logic and add load balancer support to gateway server

--- gateway-server/src/main/java/com/abcham/gatewayserver/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/abcham/gatewayserver/GatewayServerApplication.java
@@ -2,12 +2,45 @@ package com.abcham.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GatewayServerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServerApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(p -> p
+						.path("/ACCOUNTS/**")
+						.filters(f -> f.rewritePath("/ACCOUNTS/(?<segment>.*)", "/${segment}"))
+						.uri("lb://ACCOUNTS"))
+				.route(p -> p
+						.path("/accounts/**")
+						.filters(f -> f.rewritePath("/accounts/(?<segment>.*)", "/${segment}"))
+						.uri("lb://ACCOUNTS"))
+				.route(p -> p
+						.path("/CARDS/**")
+						.filters(f -> f.rewritePath("/CARDS/(?<segment>.*)", "/${segment}"))
+						.uri("lb://CARDS"))
+				.route(p -> p
+						.path("/cards/**")
+						.filters(f -> f.rewritePath("/cards/(?<segment>.*)", "/${segment}"))
+						.uri("lb://CARDS"))
+				.route(p -> p
+						.path("/LOANS/**")
+						.filters(f -> f.rewritePath("/LOANS/(?<segment>.*)", "/${segment}"))
+						.uri("lb://LOANS"))
+				.route(p -> p
+						.path("/loans/**")
+						.filters(f -> f.rewritePath("/loans/(?<segment>.*)", "/${segment}"))
+						.uri("lb://LOANS"))
+				.build();
+	}
 
 }
