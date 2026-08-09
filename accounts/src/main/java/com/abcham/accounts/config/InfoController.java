dfb707264a1ca5b809c7a6fb424f6f7bc73fa6dd
cham207388 <cham207388@gmail.com>
2026-08-09 16:02:54 -0500

    rate limiting

--- accounts/src/main/java/com/abcham/accounts/config/InfoController.java
+++ accounts/src/main/java/com/abcham/accounts/config/InfoController.java
@@ -1,12 +1,12 @@
 package com.abcham.accounts.config;
 
-import com.abcham.accounts.dto.CustomerDetailsDto;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/accounts")
-public class ContactInfoController {
+public class InfoController {
 
     private final ContactInfo contactInfo;
+    private final Environment environment;
 
     @GetMapping("/contact-info")
     public ResponseEntity<ContactInfo> getContactInfo() {
@@ -37,5 +38,16 @@ public class ContactInfoController {
     public ResponseEntity<String> getBuildInfoFallback(Throwable throwable) {
         log.debug("getBuildInfoFallback() method Invoked");
         return ResponseEntity.ok("0.9");
+    }
+
+    @GetMapping("/java-version")
+    @RateLimiter(name = "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
+    public ResponseEntity<String> getJavaVersion() {
+        return ResponseEntity.ok(environment.getProperty("JAVA_HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable) {
+        log.debug("getJavaVersionFallback() method Invoked");
+        return ResponseEntity.ok("25");
     }
 }
