22243e488e8c3731993179be93bbf571217be069
cham207388 <cham207388@gmail.com>
2026-08-09 14:07:57 -0500

    retry

--- accounts/src/main/java/com/abcham/accounts/config/ContactInfoController.java
+++ accounts/src/main/java/com/abcham/accounts/config/ContactInfoController.java
@@ -1,24 +1,41 @@
 package com.abcham.accounts.config;
 
+import com.abcham.accounts.dto.CustomerDetailsDto;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @Getter
 @Setter
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/accounts")
 public class ContactInfoController {
 
     private final ContactInfo contactInfo;
 
-    @GetMapping("/api/accounts/contact-info")
+    @GetMapping("/contact-info")
     public ResponseEntity<ContactInfo> getContactInfo() {
 
         return ResponseEntity.ok(contactInfo);
     }
 
+    @GetMapping("/build-info")
+    @Retry(name = "getBuildInfo",fallbackMethod = "getBuildInfoFallback")
+    public ResponseEntity<String> getCustomerDetailsDto() {
+        return ResponseEntity.ok("buildVersion");
+    }
+
+    public ResponseEntity<String> getBuildInfoFallback(Throwable throwable) {
+        log.debug("getBuildInfoFallback() method Invoked");
+        return ResponseEntity.ok("0.9");
+    }
 }
--- gateway-server/src/main/java/com/abcham/gatewayserver/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/abcham/gatewayserver/GatewayServerApplication.java
@@ -5,16 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+
+import java.time.Duration;
 
 @SpringBootApplication
 public class GatewayServerApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
+
         SpringApplication.run(GatewayServerApplication.class, args);
     }
 
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+
         return routeLocatorBuilder.routes()
                 .route(p -> p
                         .path("/accounts/**", "/ACCOUNTS/**")
@@ -24,18 +29,33 @@ public class GatewayServerApplication {
                                         .setName("accountsCircuitBreaker")
                                         .setFallbackUri("forward:/accounts-fallback")
                                 )
+                                .retry(retryConfig -> retryConfig
+                                        .setRetries(3)
+                                        .setMethods(HttpMethod.GET)
+                                        .setBackoff(Duration.ofMillis(100L), Duration.ofSeconds(1), 2, true)
+                                )
                         )
                         .uri("lb://ACCOUNTS"))
                 .route(p -> p
                         .path("/cards/**", "/CARDS/**")
                         .filters(f -> f.rewritePath("(?i)/cards/(?<segment>.*)", "/${segment}")
                                 .addResponseHeader("X-Response-Time", String.valueOf(System.currentTimeMillis()))
+                                .retry(retryConfig -> retryConfig
+                                        .setRetries(3)
+                                        .setMethods(HttpMethod.GET)
+                                        .setBackoff(Duration.ofMillis(100L), Duration.ofSeconds(1), 2, true)
+                                )
                         )
                         .uri("lb://CARDS"))
                 .route(p -> p
                         .path("/loans/**", "/LOANS/**")
                         .filters(f -> f.rewritePath("(?i)/loans/(?<segment>.*)", "/${segment}")
                                 .addResponseHeader("X-Response-Time", String.valueOf(System.currentTimeMillis()))
+                                .retry(retryConfig -> retryConfig
+                                        .setRetries(3)
+                                        .setMethods(HttpMethod.GET)
+                                        .setBackoff(Duration.ofMillis(100L), Duration.ofSeconds(1), 2, true)
+                                )
                         )
                         .uri("lb://LOANS"))
                 .build();
