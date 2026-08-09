2f2e8ebf3659f82b5c9745d8c732a83bef627402
cham207388 <cham207388@gmail.com>
2026-08-09 12:48:33 -0500

    feat: implement fallback mechanism for Feign clients and add null checks in customer service details retrieval

--- accounts/src/main/java/com/abcham/accounts/service/client/CardsFeignClient.java
+++ accounts/src/main/java/com/abcham/accounts/service/client/CardsFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "cards", path = "/api/cards")
+@FeignClient(name = "cards", path = "/api/cards", fallback = CardsFallback.class)
 public interface CardsFeignClient {
 
     @GetMapping(value = "/fetch", consumes = "application/json")
--- accounts/src/main/java/com/abcham/accounts/service/client/LoansFeignClient.java
+++ accounts/src/main/java/com/abcham/accounts/service/client/LoansFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "loans", path = "/api/loans")
+@FeignClient(name = "loans", path = "/api/loans", fallback = CardsFallback.class)
 public interface LoansFeignClient {
 
     @GetMapping(value = "/fetch", consumes = "application/json")
