85e04dafe961fef9797ee9f2ad94bb68982d0660
cham207388 <cham207388@gmail.com>
2026-08-09 12:13:21 -0500

    feat: add resilience4j circuit breaker support and configuration to gateway routes

--- gateway-server/src/main/java/com/abcham/gatewayserver/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/abcham/gatewayserver/GatewayServerApplication.java
@@ -9,38 +9,43 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class GatewayServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServerApplication.class, args);
+    }
 
-	@Bean
-	public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
-		return routeLocatorBuilder.routes()
-				.route(p -> p
-						.path("/ACCOUNTS/**")
-						.filters(f -> f.rewritePath("/ACCOUNTS/(?<segment>.*)", "/${segment}"))
-						.uri("lb://ACCOUNTS"))
-				.route(p -> p
-						.path("/accounts/**")
-						.filters(f -> f.rewritePath("/accounts/(?<segment>.*)", "/${segment}"))
-						.uri("lb://ACCOUNTS"))
-				.route(p -> p
-						.path("/CARDS/**")
-						.filters(f -> f.rewritePath("/CARDS/(?<segment>.*)", "/${segment}"))
-						.uri("lb://CARDS"))
-				.route(p -> p
-						.path("/cards/**")
-						.filters(f -> f.rewritePath("/cards/(?<segment>.*)", "/${segment}"))
-						.uri("lb://CARDS"))
-				.route(p -> p
-						.path("/LOANS/**")
-						.filters(f -> f.rewritePath("/LOANS/(?<segment>.*)", "/${segment}"))
-						.uri("lb://LOANS"))
-				.route(p -> p
-						.path("/loans/**")
-						.filters(f -> f.rewritePath("/loans/(?<segment>.*)", "/${segment}"))
-						.uri("lb://LOANS"))
-				.build();
-	}
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(p -> p
+                        .path("/ACCOUNTS/**")
+                        .filters(f -> f.rewritePath("/ACCOUNTS/(?<segment>.*)", "/${segment}")
+								.circuitBreaker(config -> config
+										.setName("accounts-circuit-breaker")
+										.setFallbackUri("forward:/accounts-fallback")
+								)
+						)
+                        .uri("lb://ACCOUNTS"))
+                .route(p -> p
+                        .path("/accounts/**")
+                        .filters(f -> f.rewritePath("/accounts/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://ACCOUNTS"))
+                .route(p -> p
+                        .path("/CARDS/**")
+                        .filters(f -> f.rewritePath("/CARDS/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://CARDS"))
+                .route(p -> p
+                        .path("/cards/**")
+                        .filters(f -> f.rewritePath("/cards/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://CARDS"))
+                .route(p -> p
+                        .path("/LOANS/**")
+                        .filters(f -> f.rewritePath("/LOANS/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://LOANS"))
+                .route(p -> p
+                        .path("/loans/**")
+                        .filters(f -> f.rewritePath("/loans/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://LOANS"))
+                .build();
+    }
 
 }
